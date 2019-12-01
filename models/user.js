03379c71d1be4200cd58f9d75e2424d93096cafd
kenchen <4915185-kenchen0625@users.noreply.gitlab.com>
2019-12-01 13:54:45 +0800

    Add model and migration for User

---
+++ models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    api_token: DataTypes.STRING,
+    admin: DataTypes.BOOLEAN
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
