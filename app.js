922c899af595b18e88fc479e118515d9358193c1
kenchen <4915185-kenchen0625@users.noreply.gitlab.com>
2019-12-01 12:04:15 +0800

    Install all required packages and create a server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+
+const accessLogStream = fs.createWriteStream(path.join('logs', 'access.log'), { flags: 'a' })
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Hello world"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
